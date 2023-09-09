84756d52dbabc0330d0a9e1790a039a299a79144
Thiru <dmthiruna@gmail.com>
2023-09-09 18:22:55 +0530

    initial backend commit

---
+++ explore-backend/src/main/java/com/nextgen/explore/ExploreApplication.java
@@ -0,0 +1,13 @@
+package com.nextgen.explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreApplication.class, args);
+	}
+
+}
---
+++ explore-backend/src/test/java/com/nextgen/explore/ExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextgen.explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
